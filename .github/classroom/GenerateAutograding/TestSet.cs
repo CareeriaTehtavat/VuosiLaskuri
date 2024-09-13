b5bd5d99134ac8fb23a443333a32f1e09654aadc
Artemii Korshunov <azatotgame@gmail.com>
2024-09-13 11:30:42 +0300

    added grade

--- .github/classroom/GenerateAutograding/TestSet.cs
+++ .github/classroom/GenerateAutograding/TestSet.cs
@@ -9,7 +9,7 @@ internal sealed class TestSet
     public IReadOnlyList<Test> Tests => new Test[]
     {
         
-        new DotnetTestGroup("MyNamePrinting")
+        new DotnetTestGroup("ArtPrinting")
         {
             Points = 100
         },
