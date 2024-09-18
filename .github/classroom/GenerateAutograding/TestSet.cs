2dd3b5ac2a35134394c5c1bdc9447b5ea2987ff7
Aretemii Korshunov <56575740+TvoiaJopa@users.noreply.github.com>
2024-09-18 13:07:05 +0300

    Update TestSet.cs

--- .github/classroom/GenerateAutograding/TestSet.cs
+++ .github/classroom/GenerateAutograding/TestSet.cs
@@ -9,7 +9,7 @@ internal sealed class TestSet
     public IReadOnlyList<Test> Tests => new Test[]
     {
         
-        new DotnetTestGroup("PeruslaskutNumeromuuttujilla")
+        new DotnetTestGroup("VuosiLaskuri")
         {
             Points = 100
         },
